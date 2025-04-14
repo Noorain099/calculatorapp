f910ca61b5d3d929e6051d399aa38f0687d533d0
Noorain099 <amatulghaninoorain@gmail.com>
2025-04-14 21:37:52 +0530

    modified operators can't be selected as the first digit

--- lib/provider/calculatorprovider.dart
+++ lib/provider/calculatorprovider.dart
@@ -3,6 +3,9 @@ import 'package:function_tree/function_tree.dart';
 
 class Calculatorprovider extends ChangeNotifier{
   final controllerText = TextEditingController();
+  
+
+   
 
   setValue(String value){
     String str = controllerText.text;
@@ -14,7 +17,17 @@ class Calculatorprovider extends ChangeNotifier{
           controllerText.text = str.substring(0,str.length -1);
           break;
         case "X" :
-          controllerText.text += "*";
+          if(str.isNotEmpty){
+            controllerText.text += "*";
+            }
+          break;
+        case "/" :
+        case "+" :
+        case "%" :  
+          if(str.isNotEmpty && RegExp(r'^-?\d+?$').hasMatch(str)){
+            controllerText.text += value;
+          }
+          
           break;
         case "=" :
           compute();
@@ -26,6 +39,7 @@ class Calculatorprovider extends ChangeNotifier{
          
     }
   }
+  
 
   compute(){
     String text = controllerText.text;
